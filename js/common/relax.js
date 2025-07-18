c7dddd97bedcf325fcb88356f63413081174289c
Sabiq Alfajri <salnafsabiq@gmail.com>
2025-07-18 18:11:03 +0700

    update html

---
+++ js/common/relax.js
@@ -0,0 +1,11 @@
+import Relax from "relax";
+
+new Relax('.relax', {
+    speed: '-1',
+    center: false,
+    center: false,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false,
+})
